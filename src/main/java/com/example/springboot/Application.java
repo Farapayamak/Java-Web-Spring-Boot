adf03195d3edf2f62d3033d733cf143009df71b3
amhome <amirhosseinmehrvarzi@gmail.com>
2022-11-24 13:21:25 +0330

    basic soap with SEND web service

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,13 +1,32 @@
 package com.example.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.farapayamak.services.SoapService;
 
 @SpringBootApplication
-public class Application {
+@ComponentScan({"com.farapayamak.services"})
+public class Application implements CommandLineRunner {
+
+	private SoapService soapService;
+
+	public Application(SoapService soapService) {
+        this.soapService = soapService;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// SOAP
+		System.out.println(soapService.GetCredit());
 	}
 
 }
---
+++ src/main/java/com/farapayamak/services/SoapService.java
@@ -0,0 +1,111 @@
+package com.farapayamak.services;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+
+@Service
+public class SoapService {
+
+    private final RestTemplate restTemplate;
+
+    private String Username = "username";
+    private String Password = "password";
+
+    private String SEND_Endpoint = "http://api.payamak-panel.com/post/send.asmx/%s?username=%s&password=%s";
+    
+
+    public SoapService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    private String SetCredentials(String endpoint, String operator) {
+        return String.format(endpoint, operator, Username, Password);
+    }
+
+    // SEND
+    public String GetCredit() {
+        String operation = new Object(){}.getClass().getEnclosingMethod().getName();
+        String url = SetCredentials(SEND_Endpoint, operation);
+        return InspectResponse(this.restTemplate.getForObject(url, String.class), operation, "double");
+    }
+
+
+    public String ObjectToString(Object obj) {
+        return "&" + Arrays.stream(obj.getClass().getFields())
+        .map(f-> {
+            try {
+                return f.getName() + "=" + f.get(obj).toString();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).filter(Objects::nonNull).collect(Collectors.joining("&"));
+    }
+
+
+
+
+
+    public String InspectResponse(String response, String operation, String field) {
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+		try {
+  
+			// optional, but recommended
+			// process XML securely, avoid attacks like XML External Entities (XXE)
+			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+  
+			// parse XML file
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			InputSource is = new InputSource(new StringReader(response));
+			Document doc = db.parse(is);
+  
+			// optional, but recommended
+			// http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+			doc.getDocumentElement().normalize();
+			
+			if(doc.getDocumentElement().getNodeName() == field)
+				return doc.getDocumentElement().getTextContent();
+  
+			NodeList list = doc.getElementsByTagName(operation + "Response");
+  
+			for (int temp = 0; temp < list.getLength(); temp++) {
+  
+				Node node = list.item(temp);
+  
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+  
+					Element element = (Element) node;
+					
+					if(node.getNodeName() == field)
+						return element.getElementsByTagName(field).item(0).getTextContent();  
+				}
+			}
+  
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+
+		return response;
+	}
+}
